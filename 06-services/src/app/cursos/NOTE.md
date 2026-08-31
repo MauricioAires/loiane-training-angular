be9d68fb1b2db857e52f14b2cba4d3ebcefa1778
maf.consultoria <maf.consultoria@wefin.com.br>
2026-08-31 09:46:00 -0300

    :sparkles: update notes on dependency injection and service instantiation methods

--- 06-services/src/app/cursos/NOTE.md
+++ 06-services/src/app/cursos/NOTE.md
@@ -1,3 +1,16 @@
 # Notes
 
 Nessa aula vamos estudar o porque devemos utilizar serviços
+
+Injeção de Dependências (DI) + como usar um serviço em um
+componente.
+
+O que é uma dependências? EcmaScript 2015.
+
+DI = criar uma instância de uma classe automaticamente.
+
+Essas são as 3 formas de instanciar uma classe.
+
+1. Constructors
+2. Métodos setters
+3. Atributos
--- 06-services/src/app/cursos/cursos.ts
+++ 06-services/src/app/cursos/cursos.ts
@@ -4,20 +4,25 @@ import { Component, OnInit } from '@angular/core';
 // ECMAScript 2025
 import { CursosService } from './cursos.service';
 
+/**
+ * Services são declarados dentro de providers.
+ */
 @Component({
   imports: [NgFor],
+  providers: [CursosService],
   selector: 'app-cursos',
   styleUrl: './cursos.scss',
   templateUrl: './cursos.html',
 })
 export class Cursos implements OnInit {
-  cursosService: CursosService;
+  // cursosService: CursosService;
 
   cursos: string[] = [];
 
   // primeiro método chamado
-  constructor() {
-    this.cursosService = new CursosService();
+  constructor(private cursosService: CursosService) {
+    // this.cursosService = _cursosService;
+    // this.cursosService = new CursosService();
   }
 
   ngOnInit(): void {
