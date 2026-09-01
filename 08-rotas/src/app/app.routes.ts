33b59e393486feec05491c0ec6243ee3dbdb2d35
maf.consultoria <maf.consultoria@wefin.com.br>
2026-09-01 15:38:54 -0300

    :sparkles: update navigation structure and define application routes for Home, Login, and Courses components

--- 08-rotas/src/app/app.routes.ts
+++ 08-rotas/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import type { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home/home').then((c) => c.Home),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login').then((c) => c.Login),
+  },
+  {
+    path: 'courses',
+    loadComponent: () => import('./courses/courses').then((c) => c.Courses),
+  },
+];
