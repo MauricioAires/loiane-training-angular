f07ffdaa7ea78b10b0390039149cb875b2bdd7b7
maf.consultoria <maf.consultoria@wefin.com.br>
2026-09-01 11:28:02 -0300

    :sparkles: add FiltroArrayPipe implementation and integrate it into ExemplosPipes component; update template for filtering functionality

--- 07-pipes/src/app/exemplos-pipes/exemplos-pipes.ts
+++ 07-pipes/src/app/exemplos-pipes/exemplos-pipes.ts
@@ -1,9 +1,20 @@
 import { CurrencyPipe, DatePipe, JsonPipe, LowerCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { CamelCasePipe } from '../pipes/camel-case/camel-case-pipe';
+import { FiltroArrayPipe } from '../pipes/filtro-array/filtro-array-pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [UpperCasePipe, CurrencyPipe, DatePipe, JsonPipe, LowerCasePipe, CamelCasePipe],
+  imports: [
+    UpperCasePipe,
+    CurrencyPipe,
+    DatePipe,
+    JsonPipe,
+    LowerCasePipe,
+    CamelCasePipe,
+    FiltroArrayPipe,
+    FormsModule,
+  ],
   selector: 'app-exemplos-pipes',
   styleUrl: './exemplos-pipes.scss',
   templateUrl: './exemplos-pipes.html',
@@ -17,4 +28,19 @@ export class ExemplosPipes {
     dataLancamento: new Date(2016, 5, 23),
     url: 'http://a.co/glqjpRP',
   };
+
+  livros: string[] = ['Java', 'Angular 2'];
+
+  filtro!: string;
+
+  protected addCurso(novoLivro: any): void {
+    this.livros.push(novoLivro);
+  }
 }
+
+/**
+ * !IMPORTANTE
+ *
+ * Não faça filtro de array no template, isso deve ser
+ * feito na classe.
+ */
---
+++ 07-pipes/src/app/pipes/filtro-array/filtro-array-pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filtroArray',
+})
+export class FiltroArrayPipe implements PipeTransform {
+  transform(value: string[], args: string): string[] {
+    if (value.length === 0 || args === undefined) {
+      return value;
+    }
+
+    const filter = args.toLocaleLowerCase();
+
+    return value.filter((v) => v.toLocaleLowerCase().indexOf(filter) !== -1);
+  }
+}
+
+/**
+ *
+ * O QUE E UM PIPE PURO? É AQUELE QUE NÃO OLHA AS
+ * MODIFICAÇÕES DO VALOR QUE É PARADO COMO PARÂMETRO
+ * QUALQUER VALOR PRIMITIVO OU NÃO, NÃO RESPONDE AS MUDANÇAS DO OBJETO
+ */
