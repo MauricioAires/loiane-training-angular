e604b1fc3ce8ecdb8ad20569afe94318acaed59c
maf.consultoria <maf.consultoria@wefin.com.br>
2026-09-02 17:01:38 -0300

    :sparkles: update Courses component to handle pagination with query parameters; modify template to display current page and add next page button

--- 08-rotas/src/app/courses/courses.ts
+++ 08-rotas/src/app/courses/courses.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { ActivatedRoute, RouterLinkWithHref } from '@angular/router';
+import { Component, OnInit, signal, DestroyRef } from '@angular/core';
+import { ActivatedRoute, Router, RouterLinkWithHref } from '@angular/router';
 import { CoursesService } from '../shared/services/courses/courses';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   imports: [RouterLinkWithHref],
@@ -10,10 +11,32 @@ import { CoursesService } from '../shared/services/courses/courses';
 })
 export class Courses implements OnInit {
   protected courses = signal<any>([]);
+  protected page = signal<number>(0);
 
-  constructor(private coursesService: CoursesService) {}
+  constructor(
+    private coursesService: CoursesService,
+    private activatedRoute: ActivatedRoute,
+    private destroyRef: DestroyRef,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
     this.courses.set(this.coursesService.getCourses());
+
+    this.activatedRoute.queryParams
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((params: Record<string, number>) => {
+        this.page.set(Number(params['pagina']));
+      });
+  }
+
+  protected nextPage(): void {
+    // this.page.update((state) => state + 1);
+
+    this.router.navigate(['/', 'courses'], {
+      queryParams: {
+        pagina: this.page() + 1,
+      },
+    });
   }
 }
