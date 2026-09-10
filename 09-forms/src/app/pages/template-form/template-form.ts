d19e647cfe6f00e0ceaa5ca95ce3c60cfcac8c83
maf.consultoria <maf.consultoria@wefin.com.br>
2026-09-10 12:13:52 -0300

    :sparkles: implement template-driven form with validation and submission handling

--- 09-forms/src/app/pages/template-form/template-form.ts
+++ 09-forms/src/app/pages/template-form/template-form.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+/**
+ * FormsModule é o modulo utilizado
+ * para o template form
+ *
+ * template form => equals forms defined on template
+ */
 
 @Component({
   selector: 'app-template-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './template-form.html',
   styleUrl: './template-form.scss',
 })
 export class TemplateForm {
+  protected model = signal({
+    name: '',
+    email: '',
+  });
 
+  protected onSubmit(form: any): void {
+    console.log(form);
+  }
 }
