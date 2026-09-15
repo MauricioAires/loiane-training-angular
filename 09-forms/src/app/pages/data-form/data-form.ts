ad67a2b27f0e6b64144000f18d9ff3c253e15b43
maf.consultoria <maf.consultoria@wefin.com.br>
2026-09-15 15:12:15 -0300

    :sparkles: add onSubmit method to handle form submission and send data via HTTP POST request

--- 09-forms/src/app/pages/data-form/data-form.ts
+++ 09-forms/src/app/pages/data-form/data-form.ts
@@ -1,24 +1,23 @@
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { Component, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, DestroyRef, OnInit, signal } from '@angular/core';
 import { FormDebug } from '../../shared/form-debug/form-debug';
-import { FieldControl } from '../../shared/field-control/field-control';
+import { HttpClient } from '@angular/common/http';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-data-form',
-  imports: [ReactiveFormsModule, FormDebug, FieldControl],
+  imports: [ReactiveFormsModule, FormDebug],
   templateUrl: './data-form.html',
   styleUrl: './data-form.scss',
 })
 export class DataForm implements OnInit {
   protected form = signal<FormGroup>({} as FormGroup);
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private destroyRef: DestroyRef,
+  ) {}
 
   // sempre que o componente for inicializado.
   public ngOnInit(): void {
@@ -41,5 +40,25 @@ export class DataForm implements OnInit {
         email: [null, [Validators.required]],
       }),
     );
+  }
+
+  protected onSubmit(): void {
+    // console.log(this.form().value);
+
+    this.http
+      .post('https://jsonplaceholder.typicode.com/posts', JSON.stringify(this.form().value), {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 }
